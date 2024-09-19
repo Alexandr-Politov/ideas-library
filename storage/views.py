c31986e844969d41e0363d6f9317205c54573285
Alexandr Politov <apolitov83@gmail.com>
2024-09-19 12:46:33 +0300

    view for the index page created

--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from storage.models import Idea, Category
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_ideas = Idea.objects.count()
+    num_users = get_user_model().objects.count()
+    num_categories = Category.objects.count()
+    context = {
+        "num_ideas": num_ideas,
+        "num_users": num_users,
+        "num_categories": num_categories
+    }
+    return render(request, "storage/index.html", context)
