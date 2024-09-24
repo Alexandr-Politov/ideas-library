1cbd47bce952e09beee7c0841a8d5cbad3d625b2
Alexandr Politov <apolitov83@gmail.com>
2024-09-24 21:47:36 +0300

    ideas-library.onrender.com added to allowed hosts

--- ideas_library/settings.py
+++ ideas_library/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://ideas-library.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "ideas-library.onrender.com"]
 
 
 # Application definition
